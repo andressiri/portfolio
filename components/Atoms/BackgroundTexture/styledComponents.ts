e1d53c4eb24157e4120bd6b070c08c799ff580d8
Andrés Siri <andres.siri@hotmail.com>
2023-03-09 18:31:21 -0300

    PF Create background texture component

---
+++ components/Atoms/BackgroundTexture/styledComponents.ts
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material/styles";
+
+export const TextureContainer = styled("div", {
+  shouldForwardProp: (prop) => !["imageSrc", "zIndex"].includes(prop as string),
+})<{ imageSrc: string; zIndex: number }>(({ imageSrc, zIndex }) => ({
+  zIndex,
+  position: "absolute",
+  inset: "0px",
+  background: `url(${imageSrc}) repeat`,
+}));
--- components/Atoms/index.ts
+++ components/Atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as ContactButton } from "./ContactButton";
 export { default as NavDrawerController } from "./NavDrawerController";
+export { default as BackgroundTexture } from "./BackgroundTexture";
 export { default as StyledButton } from "./StyledButton";
