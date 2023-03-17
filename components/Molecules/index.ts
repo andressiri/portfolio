cce55efa5e612fd48ec174265a0db44edd51b988
Andrés Siri <andres.siri@hotmail.com>
2023-03-17 19:53:46 -0300

    PF Create theme mode selector component

--- components/Molecules/index.ts
+++ components/Molecules/index.ts
@@ -2,6 +2,7 @@ export { default as Avatar } from "./Avatar";
 export { default as CVButtons } from "./CVButtons";
 export { default as HeroSocialMedia } from "./HeroSocialMedia";
 export { default as LanguageSelector } from "./LanguageSelector";
+export { default as ModeSelector } from "./ModeSelector";
 export { default as NavLinks } from "./NavLinks";
 export { default as NavLogo } from "./NavLogo";
 export { default as WhatsAppFloating } from "./WhatsAppFloating";
