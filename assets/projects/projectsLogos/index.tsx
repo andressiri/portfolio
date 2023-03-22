6a1ef61f96cc4efed4ef97a8f7f5a98c76e90bb8
Andrés Siri <andres.siri@hotmail.com>
2023-03-22 00:14:49 -0300

    PF Add projects assets

---
+++ assets/projects/projectsLogos/index.tsx
@@ -0,0 +1,11 @@
+import PaidIcon from "@mui/icons-material/Paid";
+import FootstrapLogo from "./FootstrapLogo";
+import QuotesMachineLogo from "./QuotesMachineLogo";
+import RominarLogo from "./RominarLogo";
+
+export const projectsLogosArray: JSX.Element[] = [
+  <RominarLogo key="RominarLogo" />,
+  <QuotesMachineLogo key="QuotesMachineLogo" />,
+  <PaidIcon key="SpendsCheckerLogo" />,
+  <FootstrapLogo key="FootstrapLogo" />,
+];
