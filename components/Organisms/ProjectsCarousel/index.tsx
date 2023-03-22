47c6c8acb80573c2a9f1fc5eba3cc8825e977325
Andrés Siri <andres.siri@hotmail.com>
2023-03-22 00:16:13 -0300

    PF Add projects carousel custom bullets

--- components/Organisms/ProjectsCarousel/index.tsx
+++ components/Organisms/ProjectsCarousel/index.tsx
@@ -1,19 +1,53 @@
 import { FC } from "react";
 import { useTheme } from "@mui/material";
+import { projectsLogosArray } from "assets/projects";
 import { Carousel } from "components/Organisms";
 
 const ProjectsCarousel: FC = () => {
   const theme = useTheme();
   return (
-    <Carousel
-      cardWidth={1460}
-      cardHeight={600}
-      color={theme.palette.secondary.contrastText}
-    >
-      <div style={{ width: "100%", height: "100%", background: "red" }}></div>
-      <div style={{ width: "100%", height: "100%", background: "green" }}></div>
-      <div style={{ width: "100%", height: "100%", background: "blue" }}></div>
-    </Carousel>
+    <div style={{ width: "1600px" }}>
+      <Carousel
+        cardWidth={1460}
+        cardHeight={580}
+        color={theme.palette.secondary.contrastText}
+        customBullets={{
+          array: projectsLogosArray,
+          container: {
+            width: "100%",
+            height: "70px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "30px",
+          },
+          bulletContainer: {
+            width: "35px",
+            height: "35px",
+            borderRadius: "50%",
+            cursor: "pointer",
+            "& svg": {
+              width: "35px",
+              height: "35px",
+              color: theme.palette.primary.dark,
+            },
+          },
+        }}
+        auto={false}
+        initialSlide={4}
+      >
+        <div style={{ width: "100%", height: "100%", background: "red" }}></div>
+        <div
+          style={{ width: "100%", height: "100%", background: "orange" }}
+        ></div>
+        <div
+          style={{ width: "100%", height: "100%", background: "green" }}
+        ></div>
+        <div
+          style={{ width: "100%", height: "100%", background: "blue" }}
+        ></div>
+      </Carousel>
+    </div>
   );
 };
 
