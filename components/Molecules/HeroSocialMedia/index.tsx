5dc4c2a3b79baef656925f8612665c59bbf0a4f0
Andrés Siri <andres.siri@hotmail.com>
2023-04-13 18:07:43 -0300

    PF Use icon with tooltip component at hero social media

--- components/Molecules/HeroSocialMedia/index.tsx
+++ components/Molecules/HeroSocialMedia/index.tsx
@@ -1,57 +1,53 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import { useTranslation } from "next-i18next";
+import { useTheme } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import { GITHUB_URL, LINKEDIN_URL } from "config/constants";
-import { Tooltip } from "components/Atoms";
+import { IconWithTooltip } from "components/Atoms";
 import { Container, StyledAnchor } from "./styledComponents";
 
 const HeroSocialMedia: FC = () => {
-  const [linkedInTooltip, setLinkedInTooltip] = useState<boolean>(false);
-  const [gitHubTooltip, setGitHubTooltip] = useState<boolean>(false);
   const { t } = useTranslation("buttons");
+  const theme = useTheme();
+
   return (
     <Container>
       <StyledAnchor
         href={LINKEDIN_URL}
         target="_blank"
         rel="noopener noreferrer"
-        onMouseEnter={() => setLinkedInTooltip(true)}
-        onMouseLeave={() => setLinkedInTooltip(false)}
         aria-label={t("linkedIn") as string}
       >
-        {linkedInTooltip ? (
-          <Tooltip
-            top={"-28px"}
-            left={"-19px"}
-            width={"90px"}
-            color={"contrastsSecondary"}
-          >
-            {t("linkedIn") as string}
-          </Tooltip>
-        ) : (
-          <></>
-        )}
-        <LinkedInIcon
-          sx={{ transform: "scale(1.15) translate(4.5px, 0.5px)" }}
-        ></LinkedInIcon>
+        <IconWithTooltip
+          icon={
+            <LinkedInIcon
+              sx={{ transform: "scale(1.15) translate(4.5px, 0.5px)" }}
+            />
+          }
+          color={theme.palette.secondary.contrastText}
+          background="transparent"
+          tooltipText={t("linkedIn") as string}
+          tooltipProps={{
+            width: "90px",
+            color: "contrastsSecondary",
+            left: "-19px",
+          }}
+        />
       </StyledAnchor>
       <StyledAnchor
         href={GITHUB_URL}
         target="_blank"
         rel="noopener noreferrer"
-        onMouseEnter={() => setGitHubTooltip(true)}
-        onMouseLeave={() => setGitHubTooltip(false)}
         aria-label={t("gitHub") as string}
       >
-        {gitHubTooltip ? (
-          <Tooltip top={"-29px"} width={"90px"} color="contrastsSecondary">
-            {t("gitHub") as string}
-          </Tooltip>
-        ) : (
-          <></>
-        )}
-        <GitHubIcon />
+        <IconWithTooltip
+          icon={<GitHubIcon />}
+          color={theme.palette.secondary.contrastText}
+          background="transparent"
+          tooltipText={t("gitHub") as string}
+          tooltipProps={{ width: "90px", color: "contrastsSecondary" }}
+        />
       </StyledAnchor>
     </Container>
   );
