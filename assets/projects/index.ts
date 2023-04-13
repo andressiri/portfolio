6878ace5e8f486a5cba9eb17fe8ffbc00166362f
Andrés Siri <andres.siri@hotmail.com>
2023-04-13 15:44:02 -0300

    PF Add projects assets

--- assets/projects/index.ts
+++ assets/projects/index.ts
@@ -1,1 +1,7 @@
+import desktopScreen from "./desktopScreen.png";
+import mobilePhone from "./mobilePhone.png";
+
 export * from "./projectsLogos";
+export * from "./projectsImages";
+
+export { desktopScreen, mobilePhone };
--- assets/projects/projectsLogos/index.tsx
+++ assets/projects/projectsLogos/index.tsx
@@ -2,10 +2,14 @@ import PaidIcon from "@mui/icons-material/Paid";
 import FootstrapLogo from "./FootstrapLogo";
 import QuotesMachineLogo from "./QuotesMachineLogo";
 import RominarLogo from "./RominarLogo";
+import TreehouseLogo from "./TreehouseLogo";
+import ASLogo from "assets/logos/ASLogo";
 
 export const projectsLogosArray: JSX.Element[] = [
   <RominarLogo key="RominarLogo" />,
   <QuotesMachineLogo key="QuotesMachineLogo" />,
   <PaidIcon key="SpendsCheckerLogo" />,
   <FootstrapLogo key="FootstrapLogo" />,
+  <TreehouseLogo key="TreehouseLogo" />,
+  <ASLogo key="AndresSiriLogo" />,
 ];
