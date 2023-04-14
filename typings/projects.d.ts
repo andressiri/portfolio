ce584a3189c3b2394d412cd2c854904f12c21506
Andrés Siri <andres.siri@hotmail.com>
2023-04-14 05:39:18 -0300

    PF Create project typings

--- typings/projects.d.ts
+++ typings/projects.d.ts
@@ -1,5 +1,15 @@
+export interface ITechnology {
+  text: string;
+  logo: JSX.Element;
+}
+
 export interface IProjectData {
   title: string;
+  subtitle: string;
+  logo: JSX.Element;
+  description: string;
+  gitHubLink: string;
+  technologies: ITechnology[];
   desktopSrc?: StaticImageData | string;
   mobileSrc?: StaticImageData | string;
 }
