7c7bfcc1d1567ab474157be84351f256f3150c4e
Andrés Siri <andres.siri@hotmail.com>
2023-04-19 11:01:54 -0300

    PF Adapt options selectors to unfold upwards

--- components/Molecules/Options/index.tsx
+++ components/Molecules/Options/index.tsx
@@ -1,17 +1,25 @@
 import { FC } from "react";
 import { LanguageSelector, ModeSelector } from "components/Molecules";
 import { Container } from "./styledComponents";
+import { ContainerProps } from "typings/customSelector";
 
-interface Props {
-  isDrawer?: boolean;
-  isDrawerOpen?: boolean;
-}
-
-const Options: FC<Props> = ({ isDrawer, isDrawerOpen }) => {
+const Options: FC<ContainerProps> = ({
+  isDrawer,
+  isDrawerOpen,
+  upwardsUnfold,
+}) => {
   return (
     <Container isDrawer={isDrawer}>
-      <ModeSelector isDrawer={isDrawer} isDrawerOpen={isDrawerOpen} />
-      <LanguageSelector isDrawer={isDrawer} isDrawerOpen={isDrawerOpen} />
+      <ModeSelector
+        isDrawer={isDrawer}
+        isDrawerOpen={isDrawerOpen}
+        upwardsUnfold={upwardsUnfold}
+      />
+      <LanguageSelector
+        isDrawer={isDrawer}
+        isDrawerOpen={isDrawerOpen}
+        upwardsUnfold={upwardsUnfold}
+      />
     </Container>
   );
 };
