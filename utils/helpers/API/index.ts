c46aa4e52af2aee3ad0c71dab36ac1fc726c5f51
Andrés Siri <andres.siri@hotmail.com>
2023-04-19 09:01:13 -0300

    PF Create send emails API helper

---
+++ utils/helpers/API/index.ts
@@ -0,0 +1,1 @@
+export { default as sendEmails } from "./sendEmails";
--- utils/helpers/index.ts
+++ utils/helpers/index.ts
@@ -2,3 +2,4 @@ export { default as getBrowser } from "./getBrowser";
 export { default as getWindowDimensions } from "./getWindowDimensions";
 export { default as goToElement } from "./goToElement";
 export { default as validateEmail } from "./validateEmail";
+export * from "./API";
