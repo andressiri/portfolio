9eaafcebb63549aa86ada7ce5752b6113b5ac9a7
Andrés Siri <andres.siri@hotmail.com>
2023-04-19 08:58:18 -0300

    PF Create validate email helper

--- utils/helpers/index.ts
+++ utils/helpers/index.ts
@@ -1,3 +1,4 @@
 export { default as getBrowser } from "./getBrowser";
 export { default as getWindowDimensions } from "./getWindowDimensions";
 export { default as goToElement } from "./goToElement";
+export { default as validateEmail } from "./validateEmail";
