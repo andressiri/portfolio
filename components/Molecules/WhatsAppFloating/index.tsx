80f544d0eacaf285ca2fb422651263a37462c232
Andrés Siri <andres.siri@hotmail.com>
2023-04-20 04:59:07 -0300

    PF Update WhatsApp floating button

--- components/Molecules/WhatsAppFloating/index.tsx
+++ components/Molecules/WhatsAppFloating/index.tsx
@@ -1,54 +1,15 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-import { useTranslation } from "next-i18next";
-import { Tooltip, WhatsAppAnchor } from "components/Atoms";
-import { Container, StyledIconContainer } from "./styledComponents";
-import useDragControllers from "./useDragControllers";
+import { FloatingButton } from "components/Atoms";
+import { WHATSAPP_URL } from "config/constants";
 
 const WhatsAppFloating: FC = () => {
-  const [displayTooltip, setDisplayTooltip] = useState(false);
-  const dragControllers = useDragControllers();
-  const { top, left, touchStart, lockScrolling, isDragging, tooltipPosition } =
-    dragControllers;
-  const { t } = useTranslation("buttons");
-
   return (
-    <Container
-      onMouseEnter={() => {
-        if (isDragging.current) return;
-        setDisplayTooltip(true);
-      }}
-      onMouseLeave={() => setDisplayTooltip(false)}
-      onTouchStart={(e) => {
-        lockScrolling();
-        setDisplayTooltip(false);
-        touchStart(e);
-      }}
-      onMouseDown={(e) => {
-        e.preventDefault();
-        setDisplayTooltip(false);
-        touchStart(e);
-      }}
-      topPosition={top}
-      leftPosition={left}
-    >
-      <WhatsAppAnchor
-        onClick={(e) => {
-          if (isDragging.current) e.preventDefault();
-        }}
-      >
-        <StyledIconContainer>
-          <WhatsAppIcon />
-        </StyledIconContainer>
-        {displayTooltip ? (
-          <Tooltip tooltipPosition={tooltipPosition}>
-            {t("whatsAppContact") as string}
-          </Tooltip>
-        ) : (
-          <></>
-        )}
-      </WhatsAppAnchor>
-    </Container>
+    <FloatingButton
+      icon={<WhatsAppIcon />}
+      tooltipText="whatsAppContact"
+      href={WHATSAPP_URL}
+    />
   );
 };
 
