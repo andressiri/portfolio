0e8f119ee2f5cd4c4779ec60b2c7b20e826345fb
andressiritm <andres.siri@tiendamia.com>
2025-09-15 13:32:41 -0300

    PF Activate drawer navigation when navbar is not visible

--- components/Atoms/NavDrawerController/styledComponents.ts
+++ components/Atoms/NavDrawerController/styledComponents.ts
@@ -1,11 +1,13 @@
 import { styled } from "@mui/material/styles";
 import { Box } from "@mui/material";
 
-export const Container = styled(Box)(() => ({
-  display: "flex",
+export const Container = styled(Box, {
+  shouldForwardProp: (prop) => !["isNavbarVisible"].includes(prop as string),
+})<{ isNavbarVisible: boolean }>(({ isNavbarVisible }) => ({
+  display: isNavbarVisible ? "none" : "flex",
   alignItems: "center",
   justifyContent: "center",
-  "@media (min-width: 1150px)": {
-    display: "none",
+  "@media (max-width: 1150px)": {
+    display: "flex",
   },
 }));
